c2d4e869deb3dd8c51df600d36ce23ba8be531a3
Ravuthz <yovannaravuth@gmail.com>
2022-11-14 22:02:02 +0700

    Update CheckRam.java

--- CheckRam.java
+++ CheckRam.java
@@ -1,10 +1,12 @@
+import java.lang.management.*;
+
 public class CheckRam {
   public static void main(String[] args) {
     int mb = 1024 * 1024;
     MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
     long xmx = memoryBean.getHeapMemoryUsage().getMax() / mb;
     long xms = memoryBean.getHeapMemoryUsage().getInit() / mb;
-    LOGGER.log(Level.INFO, "Initial Memory (xms) : {0}mb", xms);
-    LOGGER.log(Level.INFO, "Max Memory (xmx) : {0}mb", xmx);
+    System.out.println("Initial Memory (xms) : " + xms + "mb");
+    System.out.println("Max Memory (xmx) : " + xmx + "mb");
   }
 }
